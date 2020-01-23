7caf10a784e489f5c455755c8dc7ff57562fa9b6
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-01-23 23:01:19 +0200

    final style changes

--- starwars/src/components/StyledCard.js
+++ starwars/src/components/StyledCard.js
@@ -1,22 +1,21 @@
 
 import styled from "styled-components";
 
-
 const StyledCard = styled.div`
   display: flex; 
   justify-content: center;
   flex-direction: column;
   text-align: center;
   align-items: center;
-  background: #D3CBB8;
+  background: linear-gradient(to right,#3A1C71,#D76D77,#FFAF7B);
   margin: 0 auto;
   margin-top: 120px;
   max-width: 1000px;
   overflow: hidden;
-  transition: ease-in-out 0.2s;
-  border: 3px solid #443E3E;
+  transition: ease-in-out 0.3s;
+  border: 3px solid #1CB5E0;
 
-
+  
     filter: blur(3px);
     &:hover {
         filter: blur(0px);
@@ -26,20 +25,42 @@ const StyledCard = styled.div`
     &:hover {
         transform: scale(1.03);
         transition: ease-in-out 0.13s;
-        box-shadow: 2px 5px 7px #323330;
+        text-shadow: 0.2em 0.2em 0.2em #544a7d;
+        opacity:1;
     }
+
+    &:hover.styledCard{
+      transform:rotateY(180deg);
+    }
+
     @media (max-width: 500px) {
         width: 85%;
     }
+
     h2 {
-        color: #323330;
+        color: #a2ab58;
         font-size: 2rem;
         text-decoration: underline;
     }
-    p {
-        font-size: 1.2rem;
-        color: #323330;
+
+    h1{
+       color: #f4791f;
+       font-size: 2rem;
+      
     }
+
+    h3 {
+      color: ${props => props.inputColor || "#19547b"};
+      text-decoration: underline ${props => props.inputColor || "#64f38c"};
+      font-size: 2rem;
+    }
+
+    p {
+        font-size: 1.3rem;
+        color: ${props => props.primary ? "#32330": "#d3cbb8"};
+           
+    }
+
     span {
         font-weight: bold;
     }
